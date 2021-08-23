cfebef89c011001bb98db030a63f0288a2716a41
saksham <saksham10.saxena@gmail.com>
2021-08-23 19:57:13 +0530

    Added functionality of Adding Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,77 @@
 import React from 'react';
 
-const App = () => {
+const App:React.FC = () => {
 
-  var tasks : string[] = ['Task 1', 'Task 2'];
+  var tasks: string[] = ['Task 1', 'Task 2'];
 
-  const [task , setTask] = React.useState<Array<string>>();
+ // type AddedTask = string;
 
- 
+  const [allTask , setAllTask] = React.useState<string[]>(tasks);
 
+  const [inputValue, setInputValue] = React.useState<string>('');
+
+  
+  const handleTask = () => {
+    setAllTask([...allTask, inputValue]);
+  }
+
+  const handleInput = event => {
+    setInputValue(event.target.value);
+  }
 
   return (
-    <div>
-       <h1>To Do List </h1><br/>
-       <h2>Tasks</h2>
-       <AllTask task={tasks} />
+    <div className="App">
+      <h1>To Do App </h1><hr />
+      <h2>Add Task</h2>
+      <input type="text" value={inputValue} onChange = {handleInput} /><br/>  
+          <button type="button" onClick={handleTask}>Add Task</button>
+      <hr />
+      <h2>All Tasks</h2>
+      <AllTask task={allTask} />
     </div>
   )
 }
 
+type TaskProp = {
+  task: string[];
+};
 
-const AllTask = (task: String) => {
-  
+
+
+const AllTask: React.FC<TaskProp> = (taskProp: TaskProp) => {
+  return (
+    <div>
+      <ul>
+        {
+          taskProp.task.map(task => <li>{task}</li>)
+        }
+      </ul>
+      {JSON.stringify(taskProp.task)}
+    </div>
+
+  )
 }
+
+// type AddingTaskProp = {
+//   addTaskProp : string;
+// }
+
+// type addTaskFn = (tasks: string[]) => void;
+
+// const AddTask = ({onAdd : addTaskFn} => {
+//   return (
+//     <div>
+//             <input id="search" type="text" />
+//     </div>
+//   )
+// })
+
+// const AddTask = {((addtask : AddingTaskProp), (onAdd : addTaskFn)) => {
+//   return (
+//     <label htmlFor="addTask" placeholder="Add Task"></label>
+//   )
+// }}
+
 
 
 
