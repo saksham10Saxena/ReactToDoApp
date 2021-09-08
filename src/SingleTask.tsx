31b996553bb6a9700c0d909563e6877dee9cc539
saksham <saksham10.saxena@gmail.com>
2021-09-08 19:03:01 +0530

    EditTask functionality Added

--- src/SingleTask.tsx
+++ src/SingleTask.tsx
@@ -8,16 +8,64 @@ type SingleTaskProp = {
 
 const SingleTask: React.FC<SingleTaskProp> = ({ singleTask, togglecheck }: SingleTaskProp) => {
   const toggleCheck = (): void => {
-  
-       singleTask.checkedStatus = !singleTask.checkedStatus
-
+    singleTask.checkedStatus = !singleTask.checkedStatus
     togglecheck(singleTask)
   }
 
-    return (
+  const [editTask, setEditTask] = React.useState<boolean>(false)
+  const [inputValueEditingTask, setInputValueEditingTask] = React.useState<string>(singleTask.title)
+
+  const handleEditingTaskInput = (event) => {
+    setInputValueEditingTask(event.target.value)
+  }
+
+  const handleEditedTaskInput = () => {
+    editMainState()
+  }
+
+  const handleKeypress = (event) => {
+    if (event.keyCode === 13) {
+      handleEditedTaskInput()
+    } else {
+      handleEditingTaskInput(event)
+    }
+  }
+
+  const editMainState = () => {
+    singleTask.title = inputValueEditingTask
+    togglecheck(singleTask)
+    setEditTask(false)
+  }
+
+  return (
     <div>
-      <input type="checkbox" checked={singleTask.checkedStatus} onChange={() => toggleCheck()} />
-      <span className={singleTask.checkedStatus ? 'background-red' : ''}>{singleTask.title}</span>
+      {editTask ? (
+        <div>
+          <input
+            type="text"
+            value={inputValueEditingTask}
+            onChange={handleEditingTaskInput}
+            onKeyDown={handleKeypress}
+          />
+          <button type="button" onClick={() => editMainState()}>
+            save
+          </button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="checkbox"
+            checked={singleTask.checkedStatus}
+            onChange={() => toggleCheck()}
+          />
+          <span className={singleTask.checkedStatus ? 'background-red' : ''}>
+            {singleTask.title}
+          </span>
+          <button type="button" onClick={() => setEditTask(true)}>
+            editTask
+          </button>
+        </div>
+      )}
     </div>
   )
 }
